7e0aefc7d8424b791a17519e37467e0b9e9b4e71
sandeepkumar530 <sandi7792@gmail.com>
2025-03-15 14:09:43 +0530

    todaywork

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { CircleComponent } from './circle/circle.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { BmiComponent } from './bmi/bmi.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'home',component:HomeComponent,children:[
+    {path:'welcome',component:WelcomeComponent},
+    {path:'calculator',component:CalculatorComponent},
+    {path:'databinding',component:DatabindingComponent},
+    {path:'circle',component:CircleComponent},
+    {path:'rectangle',component:RectangleComponent},
+    {path:'bmi',component:BmiComponent}
+  ]}
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HomeComponent } from './home/home.component';
+import { CalculatorComponent } from './calculator/calculator.component';
+import { FormsModule } from '@angular/forms';
+import { DatabindingComponent } from './databinding/databinding.component';
+import { RectangleComponent } from './rectangle/rectangle.component';
+import { CircleComponent } from './circle/circle.component';
+import { BmiComponent } from './bmi/bmi.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    WelcomeComponent,
+    HomeComponent,
+    CalculatorComponent,
+    DatabindingComponent,
+    RectangleComponent,
+    CircleComponent,
+    BmiComponent,
   ],
   imports: [
     BrowserModule,
